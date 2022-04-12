270e3d08ed062554f133f29f3fcbca3d368817b6
WilliamLiu666 <cyfyliu@gmail.com>
2022-04-12 16:46:32 +0100

    Add plots

---
+++ CCA-main.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 11 16:19:58 2022
+
+@author: WILL LIU
+"""
+
+from cca import *
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cross_decomposition import CCA
+'''
+x = np.load('balanced-MNIST.npy')
+x = x/255
+x = (x - x.mean(axis=0))
+X = x[:,300:335]
+Y = x[:,400:435]
+length = 112800
+'''
+length = 50000
+l1 = np.random.normal(size=length)
+l2 = np.random.normal(size=length)
+l3 = np.random.normal(size=length)
+
+latents = np.array([l1, l1*0.5, l1*0.25, l2*0.7, l2*0.3, l3*0.5]).T
+X = latents + np.random.normal(size=6 * length).reshape((length, 6))*0.5
+Y = latents + np.random.normal(size=6 * length).reshape((length, 6))*0.5
+
+X = X-X.mean(axis=0)
+Y = Y-Y.mean(axis=0)
+
+plt.plot(X[:200,0],label='X[:200,0]')
+plt.plot(Y[:200,0],label='Y[:200,0]')
+plt.legend()
+plt.title('first 200 points in the first column')
+plt.show()
+
+plt.plot(X[:200,1],label='X[:200,1]')
+plt.plot(Y[:200,1],label='Y[:200,1]')
+plt.legend()
+plt.title('first 200 points in the second column')
+plt.show()
+
+eta1=0.0025
+eta2=0.0005
+init_l1=0
+init_l2=0
+
+
+a,b,list1,list2,corr_list = streamingCCA(X,Y,eta1,eta2,init_l1,init_l2)
+cca = CCA(n_components=1)
+cca.fit(X, Y)
+X_c, Y_c = cca.transform(X, Y)
+
+plt.plot(np.array(list(range(1,length//100+1)))*100,corr_list,label='streaming method')
+plt.plot([100,length],[np.corrcoef(X_c.T,Y_c.T)[1,0],np.corrcoef(X_c.T,Y_c.T)[1,0]],label='built-in cca function')
+plt.xlabel('iterations')
+plt.ylabel('correlation')
+plt.legend()
+plt.title('correlation vs iteration,$\eta_1$={} $\eta_2$={}'.format(eta1,eta2))
+plt.show()
+
+plt.plot(np.array(list(range(1,length//100+1)))*100,list1,label='$\lambda_1$')
+plt.plot(np.array(list(range(1,length//100+1)))*100,list2,label='$\lambda_2$')
+plt.xlabel('iterations')
+plt.legend()
+plt.title('$\lambda$ vs iteration,$\eta_1$={} $\eta_2$={}'.format(eta1,eta2))
+plt.show()
--- cca.py
+++ cca.py
@@ -23,7 +23,7 @@ def streamingCCA(X,Y,eta1=0.0025,eta2=0.0005,init_l1=0,init_l2=0):
     list2 = []
     corr_list = []
         
-    for j in range(1128):
+    for j in range(length//100):
         for i in range(100):
     
             ind = j*100+i
@@ -39,6 +39,76 @@ def streamingCCA(X,Y,eta1=0.0025,eta2=0.0005,init_l1=0,init_l2=0):
             b += eta1*(c12.T@a-l2*(c22@b))
             l1 += eta2*(a.T@c11@a-1)
             l2 += eta2*(b.T@c22@b-1)
+    
+        X_s = X@a
+        Y_s = Y@b    
+        list1.append(float(l1.copy()))
+        list2.append(float(l2.copy()))
+        
+        corr_list.append(np.corrcoef(X_s.T,Y_s.T)[1,0])
+
+        
+    return a,b,list1,list2,corr_list
+
+def streamingCCA_modified(X,Y,eta11=0.005,eta12=0.005,eta21=0.001,eta22=0.001,init_l1=0,init_l2=0):
+    
+    length,m = X.shape
+    
+    a = np.random.randn(m,1)
+    a,_ = np.linalg.qr(a, mode='reduced')
+    b = np.random.randn(m,1)
+    b,_ = np.linalg.qr(b, mode='reduced')
+
+    l1 = init_l1
+    l2 = init_l2
+    
+    list1 = []
+    list2 = []
+    corr_list = []
+    
+    it = length//100
+        
+    for j in range(it//15):
+        for i in range(100):
+    
+            ind = j*100+i
+            
+            x = X[ind,:]
+            y = Y[ind,:]
+            
+            c12 = np.outer(x,y)
+            c11 = np.outer(x,x)
+            c22 = np.outer(y,y)
+            
+            a += eta11*(c12@b-l1*(c11@a))
+            b += eta11*(c12.T@a-l2*(c22@b))
+            l1 += eta12*(a.T@c11@a-1)
+            l2 += eta12*(b.T@c22@b-1)
+    
+        X_s = X@a
+        Y_s = Y@b    
+        list1.append(float(l1.copy()))
+        list2.append(float(l2.copy()))
+        
+        corr_list.append(np.corrcoef(X_s.T,Y_s.T)[1,0])
+        
+        
+    for j in range(it//15,it):
+        for i in range(100):
+    
+            ind = j*100+i
+            
+            x = X[ind,:]
+            y = Y[ind,:]
+            
+            c12 = np.outer(x,y)
+            c11 = np.outer(x,x)
+            c22 = np.outer(y,y)
+            
+            a += eta21*(c12@b-l1*(c11@a))
+            b += eta21*(c12.T@a-l2*(c22@b))
+            l1 += eta22*(a.T@c11@a-1)
+            l2 += eta22*(b.T@c22@b-1)
     
         X_s = X@a
         Y_s = Y@b    
@@ -85,20 +155,34 @@ if __name__=='__main__':
     import matplotlib.pyplot as plt
     from sklearn.cross_decomposition import CCA
     
-    x = np.load('balanced-MNIST.npy')
-    x = x/255
-    x = (x - x.mean(axis=0))
-    X = x[:,300:335]
-    Y = x[:,400:435]
+    length = 50000
+    l1 = np.random.normal(size=length)
+    l2 = np.random.normal(size=length)
+    l3 = np.random.normal(size=length)
+
+    latents = np.array([l1, l1*0.5, l1*0.25, l2*0.7, l2*0.3, l3*0.5]).T
+    X = latents + np.random.normal(size=6 * length).reshape((length, 6))*0.5
+    Y = latents + np.random.normal(size=6 * length).reshape((length, 6))*0.5
+
+    X = X-X.mean(axis=0)
+    Y = Y-Y.mean(axis=0)
     
-    a,b,list1,list2,corr_list = streamingCCA(X, Y)
+    a,b,list1,list2,corr_list = streamingCCA_modified(X, Y,eta11=0.005,eta12=0.005,eta21=0.0015,eta22=0.0015,init_l1=0,init_l2=0)
     cca = CCA(n_components=1)
     cca.fit(X, Y)
     X_c, Y_c = cca.transform(X, Y)
     
-    plt.plot(np.array(list(range(1,1129)))*100,corr_list,label='streaming method')
-    plt.plot([100,112800],[np.corrcoef(X_c.T,Y_c.T)[1,0],np.corrcoef(X_c.T,Y_c.T)[1,0]],label='built-in cca function')
+    plt.plot(np.array(list(range(1,length//100+1)))*100,corr_list,label='streaming method')
+    plt.plot([100,50000],[np.corrcoef(X_c.T,Y_c.T)[1,0],np.corrcoef(X_c.T,Y_c.T)[1,0]],label='built-in cca function')
     plt.xlabel('iterations')
     plt.ylabel('correlation')
+    plt.title('mixed learning rate')
     plt.legend()
+    plt.show()
+    
+    plt.plot(np.array(list(range(1,length//100+1)))*100,list1,label='$\lambda_1$')
+    plt.plot(np.array(list(range(1,length//100+1)))*100,list2,label='$\lambda_2$')
+    plt.xlabel('iterations')
+    plt.legend()
+    plt.title('mixed learning rate')
     plt.show()
