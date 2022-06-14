f59b885bd7ad67d9d834531242cf9f1ee6bb0dee
WilliamLiu666 <cyfyliu@gmail.com>
2022-06-14 15:50:03 +0100

    comments added

--- pca cifar.py
+++ pca cifar.py
@@ -9,15 +9,18 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from oja import *
-from gen_data import *
 
-
+#load cifar data
 x = tf.keras.datasets.cifar10.load_data()[0][0]
+
+#flatten the data
 x = x.reshape(-1,32*32*3)
 x = x/255
 n_features = 32*32*3
 num = 3
-xn = centralize(x)
+
+#centralize the data
+xn = x-np.mean(x,axis=0)
 it = 1000
 
 #Built-in PCA
@@ -25,6 +28,7 @@ pca_obj = PCA(n_components=(n_features))
 pca_obj.fit(x)
 pca = pca_obj.components_.T[:,:num]
 
+#plot the eigenvalue ratio
 fig = plt.figure()
 ax = fig.add_axes([0,0,1,1])
 ax.bar(list(range(1,11)),pca_obj.explained_variance_ratio_[:10]*100)
@@ -34,29 +38,34 @@ plt.xlabel('i th eigenvalue')
 plt.legend(['eigen value ratio','cumulative eigen value ratio'])
 plt.show()
 
+#initialzation
 error_mat = np.zeros((it,num))
 var_mat = np.zeros((it,num))
 V = init_V(n_features,num)
 var_mat[0,:] = np.diag(V.T@xn.T@xn@V/50000)
 error_mat[0,:] = np.abs(var_mat[0,:]-pca_obj.explained_variance_[:num])
 
-
+#Oja
 for i in range(it):
     print(i)
     V = ojafunc(xn[i*10:(i+1)*10,:], V ,0.0001)
     var_mat[i,:] = np.diag(V.T@xn.T@xn@V/50000)
     error_mat[i,:] = np.abs(var_mat[i,:]-pca_obj.explained_variance_[:num])
-    
-color_list=['blue','g','r']    
+
+#set plot colour
+color_list=['blue','g','r']
+
+#error plot
 for i in range(num):
     plt.plot(np.array(range(it))*10,error_mat[:,i],color=color_list[i],label='{}th'.format(i))
-
 plt.title('Error plot')
 plt.xlabel('Iteration')
 plt.ylabel('Error')
 plt.legend()
 plt.show()
 
+
+#variance plot
 for i in range(num):
     plt.plot(np.array(range(it))*10,var_mat[:,i],color=color_list[i],label='{}th streaming'.format(i))
     plt.plot([0,it*10],[pca_obj.explained_variance_[i],pca_obj.explained_variance_[i]],color=color_list[i],linestyle='dashed',label='{}th theoretical'.format(i))
--- pca two_moons.py
+++ pca two_moons.py
@@ -8,52 +8,61 @@ Created on Fri May 27 11:32:54 2022
 from sklearn import datasets
 import matplotlib.pyplot as plt
 import numpy as np
-from gen_data import *
 from sklearn.decomposition import PCA
 from oja import *
+from scipy.stats import special_ortho_group
 
+#load the two moon data
 x=datasets.make_moons(n_samples=10000)[0]
 plt.plot(x[:,0],x[:,1],'.')
 plt.title('Two Moons Dataset')
 plt.show()
 
+#rotate the data
 x = np.concatenate((x,np.zeros((10000,100))),axis=1)
-xn = centralize(x)
+r = special_ortho_group.rvs(100+2)
+x = x@r.T
+
+#centralize the data
+xn =  x-np.mean(x,axis=0)
 num = 2
 it = 1000
 
+#theoretical pca value
 pca_obj = PCA(n_components=(102))
 pca_obj.fit(x)
 pca = pca_obj.components_.T[:,:num]
 
+#initialzation
 error_mat = np.zeros((it,num))
 var_mat = np.zeros((it,num))
 V = init_V(102,num)
 var_mat[0,:] = np.diag(V.T@xn.T@xn@V/50000)
 error_mat[0,:] = np.abs(var_mat[0,:]-pca_obj.explained_variance_[:num])
 
-
+#Oja
 for i in range(it):
     print(i)
     V = ojafunc(xn[i*10:(i+1)*10,:], V ,0.005)
     var_mat[i,:] = np.diag(V.T@xn.T@xn@V/10000)
     error_mat[i,:] = np.abs(var_mat[i,:]-pca_obj.explained_variance_[:num])
     
-    
-color_list=['blue','g','r']    
+#set plot colour
+color_list=['blue','g','r']
+
+#error plot
 for i in range(num):
     plt.plot(np.array(range(it))*10,error_mat[:,i],color=color_list[i],label='{}th'.format(i))
-
 plt.title('Error plot')
 plt.xlabel('Iteration')
 plt.ylabel('Error')
 plt.legend()
 plt.show()
 
+#variance plot
 for i in range(num):
     plt.plot(np.array(range(it))*10,var_mat[:,i],color=color_list[i],label='{}th streaming'.format(i))
     plt.plot([0,it*10],[pca_obj.explained_variance_[i],pca_obj.explained_variance_[i]],color=color_list[i],linestyle='dashed',label='{}th theoretical'.format(i))
-
 plt.title('Variance plot')
 plt.xlabel('Iteration')
 plt.ylabel('Variance')
