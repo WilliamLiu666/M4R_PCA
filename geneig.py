9a9e93310a82653da6255e4c02840fd537c3b8d6
WilliamLiu666 <cyfyliu@gmail.com>
2022-06-14 15:56:32 +0100

    comments added

--- geneig.py
+++ geneig.py
@@ -7,28 +7,42 @@ Created on Sat Apr 23 17:19:25 2022
 import numpy as np
 
 def genoja(X,Y,n,eta_1=0.0005,eta_2=0.005,iterations = False):
+    '''
+    Input:
+    X,Y: data
+    n: number of target components
+    eta1,eta2: learning rate
+    iterations: max iteration. If False, run all the points in dataset
+    Output:
+    a,b: canonical correlation components
+    corr_list: correlation during the iterations
+    '''
     
+    #initialization and random starting value
     length,m = X.shape
     np.random.seed(999999999)
     V = np.random.randn(m*2,n)
     V,_ = np.linalg.qr(V, mode='reduced')
     U = np.random.randn(m*2,n)
     U,_ = np.linalg.qr(U, mode='reduced')
-    
+
+    #if iterations is not set, run all the samples in the dataset
     if iterations==False:
         max_iter = length//100
     else:
         max_iter = iterations//100
-        
     corr_mat = np.zeros((max_iter,n))
     
+    #iterations
     for j in range(max_iter):
+
+        #store the result every 100 iterations
         for i in range(100):
-            
+
+            #initialize the cov matrices
             ind = j*100+i
             x = X[ind,:]
             y = Y[ind,:]
-            
             c12 = np.outer(x,y)
             c11 = np.outer(x,x)
             c22 = np.outer(y,y)
@@ -46,10 +60,6 @@ def genoja(X,Y,n,eta_1=0.0005,eta_2=0.005,iterations = False):
             #two-step process
             U -= eta_2*(B@U-A@V)
             V += eta_1*U
-            '''
-            V,r = np.linalg.qr(V, mode='reduced')
-            V = V*np.sign(np.diagonal(r))#avoid switching sign in QR
-            '''
             a = V[:m]
             a,r = np.linalg.qr(a, mode='reduced')
             a = a*np.sign(np.diagonal(r))#avoid switching sign in QR
@@ -60,82 +70,22 @@ def genoja(X,Y,n,eta_1=0.0005,eta_2=0.005,iterations = False):
             V[:m] = a
             V[m:] = b
             
-        
+        #record the result
         a = V[:m]
         b = V[m:]
         X_s = X@a
         Y_s = Y@b
-        
         for k in range(n):
             corr_mat[j,k] = np.corrcoef(X_s.T,Y_s.T)[n+k,k]
 
     return a,b,corr_mat
 
-def genoja_modified(X,Y,n,eta_1=0.0005,eta_2=0.005):
-    
-    length,m = X.shape
-    np.random.seed(999999999)
-    V = np.random.randn(m*2,n)
-    V,_ = np.linalg.qr(V, mode='reduced')
-    U = np.random.randn(m*2,n)
-    U,_ = np.linalg.qr(U, mode='reduced')
-    
-    corr_mat = np.zeros((length//100-4,n))
-    
-    for j in range(length//100-4):
-        for i in range(100):
-            
-            ind = j*100+i
-            x = X[ind:ind+10,:]
-            y = Y[ind:ind+10,:]
-            
-            #cov matrix
-            c12 = x.T@y/10
-            c11 = x.T@x/10
-            c22 = y.T@y/10
-            
-            #initialize A
-            A = np.zeros((m*2,m*2))
-            A[:m,m:] = c12
-            A[m:,:m] = c12.T
-            
-            #initialize B
-            B = np.zeros((m*2,m*2))
-            B[0:m,0:m] = c11
-            B[m:m*2,m:m*2] = c22
-            
-            #two-step process
-            U -= eta_2*(B@U-A@V)
-            V += eta_1*U
-            '''
-            V,r = np.linalg.qr(V, mode='reduced')
-            V = V*np.sign(np.diagonal(r))#avoid switching sign in QR
-            '''
-            a = V[:m]
-            a,r = np.linalg.qr(a, mode='reduced')
-            a = a*np.sign(np.diagonal(r))#avoid switching sign in QR
-            
-            b = V[m:]
-            b,r = np.linalg.qr(b, mode='reduced')
-            b = b*np.sign(np.diagonal(r))#avoid switching sign in QR
-            V[:m] = a
-            V[m:] = b
-            
-        
-        a = V[:m]
-        b = V[m:]
-        X_s = X@a
-        Y_s = Y@b
-        
-        for k in range(n):
-            corr_mat[j,k] = np.corrcoef(X_s.T,Y_s.T)[n+k,k]
-
-    return a,b,corr_mat
 
 if __name__=='__main__':
     import matplotlib.pyplot as plt
     from sklearn.cross_decomposition import CCA
-    
+
+    #test on the sythetic data
     length = 50000
     l1 = np.random.normal(size=length)
     l2 = np.random.normal(size=length)
@@ -147,9 +97,9 @@ if __name__=='__main__':
 
     X = X-X.mean(axis=0)
     Y = Y-Y.mean(axis=0)
-    
+
+    #theoretical value
     n=3
-    
     a,b,corr_list = genoja(X,Y,n,eta_1=0.0005,eta_2=0.005)
     cca = CCA(n_components=n)
     cca.fit(X, Y)
--- oja.py
+++ oja.py
@@ -7,26 +7,24 @@ Created on Mon Jan 10 19:48:20 2022
 import numpy as np
 
 def ojafunc(X, V, eta=0.01):
-    
+    '''
+    Input:
+    X: data
+    V: initial value of V
+    eta: learning rate
+    Output:
+    V: estimated principal components
+    '''
+    #initialization
     t,length = X.shape
-    
+
+    #t step iterations
     for i in range(t):
+
+        #compute cov matrix
         x = X[i,:]
         V += eta*np.outer(x,x)@V #step 1
+        
         V,_ = np.linalg.qr(V, mode='reduced') #step 2 and step 3
         
     return V
-
-def oja2(X, V, eta, V_true):
-    
-    t,length = X.shape
-    
-    for i in range(t):
-        x = X[i,:]
-        y = np.dot(x,V)
-        x = x.reshape([len(x),1])
-        V += eta * y*(x- y*V)
-        
-        error = np.linalg.norm(np.abs(V)-np.abs(V_true)) #Since the sign may change, take the difference between abs value
-        
-    return V,error
